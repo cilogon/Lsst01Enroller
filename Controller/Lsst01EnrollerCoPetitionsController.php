540356e9dd2bddcfa913ec494bf1ce46c9378a9f
Scott Koranda <skoranda@gmail.com>
2023-01-04 14:00:54 -0600

    Add logic to promote preferred name to primary
    Add logic to promote preferred name to primary using
    the petitionerAttributes plugin point.

--- Controller/Lsst01EnrollerCoPetitionsController.php
+++ Controller/Lsst01EnrollerCoPetitionsController.php
@@ -8,6 +8,9 @@
 //
 // The following enrollment steps are implemented:
 //
+// petitionerAttributes:
+//   - promotes the Name of type preferred to be the
+//     primary name
 // provision:
 //   - invokes manualProvision() on the CO Group(s)
 
@@ -18,6 +21,49 @@ class Lsst01EnrollerCoPetitionsController extends CoPetitionsController {
   // Class name, used by Cake
   public $name = "Lsst01EnrollerCoPetitions";
   public $uses = array("CoPetition");
+
+  /**
+   * Plugin functionality following petitionerAttributes step
+   *
+   * @param Integer $id CO Petition ID
+   * @param Array $onFinish URL, in Cake format
+   */
+
+  protected function execute_plugin_petitionerAttributes($id, $onFinish) {
+    // Pull the petition and its related models.
+    $args = array();
+    $args['conditions']['CoPetition.id'] = $id;
+    $args['contain']['EnrolleeCoPerson'] = 'Name';
+
+    $petition = $this->CoPetition->find('first', $args);
+    $this->log("Petitioner Attributes: Petition is " . print_r($petition, true));
+
+    // Loop over the Name objects to find the Name of type preferred
+    // and set it to be the primary name. Note that the beforeSave()
+    // method on the Name model will take care of updating the other
+    // Name objects as necessary.
+    if(!empty($petition['EnrolleeCoPerson']['Name'])) {
+      if(is_array($petition['EnrolleeCoPerson']['Name'])) {
+        foreach($petition['EnrolleeCoPerson']['Name'] as $name) {
+          if($name['type'] == NameEnum::Preferred) {
+            $id = $name['id'];
+            $data = array();
+            $data['Name'] = $name;
+            $data['Name']['primary_name'] = true;
+
+            if($this->CoPetition->EnrolleeCoPerson->Name->save($data)) {
+              $this->log("Set primary_name true for Name with ID $id");
+            } else {
+              $this->log("ERROR: could not set primary_name true for Name with ID $id");
+            }
+            break;
+          }
+        }
+      }
+    }
+
+    $this->redirect($onFinish);
+  }
 
   /**
    * Plugin functionality following provision step
